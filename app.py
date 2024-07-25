6ec1e1d5852bda2e6a21a3dc5b0614f3f4b8b652
Eniiifeoluwa <akinolaayomide226@gmail.com>
2024-07-25 07:53:58 +0100

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 import warnings
 from imblearn.over_sampling import SMOTE
